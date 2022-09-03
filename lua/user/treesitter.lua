bca7ee99e7bd3f9b9b4b9b2a9cd4a8777a64f103
Winston Kinoti <prog.kinoti@gmail.com>
2022-09-03 13:29:15 +0300

    feat: format lua files with stylua

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,28 +1,28 @@
 local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
-	return
+  return
 end
 
-configs.setup({
-	ensure_installed = {
-        "bash",
-        "c",
-        "css",
-        "fish",
-        "html",
-        "javascript",
-        "lua",
-        "markdown",
-        "python",
-        "typescript",
-    },
-	ignore_install = { "" }, -- List of parsers to ignore installing
-	highlight = {
-		enable = true, -- false will disable the whole extension
-		disable = { "css" }, -- list of language that will be disabled
-	},
-	autopairs = {
-		enable = true,
-	},
-	indent = { enable = true, disable = { "python", "css" } },
-})
+configs.setup {
+  ensure_installed = {
+    "bash",
+    "c",
+    "css",
+    "fish",
+    "html",
+    "javascript",
+    "lua",
+    "markdown",
+    "python",
+    "typescript",
+  },
+  ignore_install = { "" }, -- List of parsers to ignore installing
+  highlight = {
+    enable = true, -- false will disable the whole extension
+    disable = { "css" }, -- list of language that will be disabled
+  },
+  autopairs = {
+    enable = true,
+  },
+  indent = { enable = true, disable = { "python", "css" } },
+}
