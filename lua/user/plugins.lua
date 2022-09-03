6baaa6e27d39dc104cacfb661fb3ee9ff058cbee
Winston Kinoti <prog.kinoti@gmail.com>
2022-09-03 03:16:32 +0300

    feat: added a color highlighter

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -144,6 +144,20 @@ return packer.startup(function(use)
             }
         end
     }
+    use {
+        "norcalli/nvim-colorizer.lua",
+        config = function()
+            require("colorizer").setup({ "css", "scss", "html", "javascript" }, {
+                RGB = true, -- #RGB hex codes
+                RRGGBB = true, -- #RRGGBB hex codes
+                RRGGBBAA = true, -- #RRGGBBAA hex codes
+                rgb_fn = true, -- CSS rgb() and rgba() functions
+                hsl_fn = true, -- CSS hsl() and hsla() functions
+                css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+                css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+            })
+        end,
+    }
 
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
