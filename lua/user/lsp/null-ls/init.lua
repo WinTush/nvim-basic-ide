e1d4519f85574e4bc7f481b3d0016c601c5f481d
Winston Kinoti <prog.kinoti@gmail.com>
2022-09-10 09:28:16 +0300

    feat: add functions to return a buffers linters and formatters

--- lua/user/lsp/null-ls/init.lua
+++ lua/user/lsp/null-ls/init.lua
@@ -7,11 +7,16 @@ end
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/code_actions
+local code_actions = null_ls.builtins.code_actions
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/hover
+local hover = null_ls.builtins.hover
 
 -- https://github.com/prettier-solidity/prettier-plugin-solidity
 null_ls.setup {
   debug = false,
   sources = {
+    -- formatters
     formatting.prettierd.with {
       extra_filetypes = { "toml" },
       extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
@@ -20,6 +25,24 @@ null_ls.setup {
     formatting.isort,
     formatting.stylua,
     formatting.google_java_format,
+    formatting.shfmt,
+    formatting.fixjson,
+
+    -- linters
     diagnostics.flake8,
+    diagnostics.write_good,
+    diagnostics.tsc,
+    diagnostics.shellcheck,
+    diagnostics.codespell,
+    diagnostics.sqlfluff.with {
+      extra_args = { "--dialect", "postgres" }, -- change to your dialect
+    },
+
+    -- code actions
+    code_actions.gitsigns,
+    code_actions.gitrebase,
+
+    -- hover
+    hover.dictionary,
   },
 }
